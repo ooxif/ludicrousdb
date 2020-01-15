10a288480a673c870f50bac420a6d5515412fbbf
oo <oo@xif.at>
2020-01-15 15:37:15 +0900

    fix: the wrong detection of is_write_query()

--- ludicrousdb/includes/class-ludicrousdb.php
+++ ludicrousdb/includes/class-ludicrousdb.php
@@ -367,13 +367,12 @@ class LudicrousDB extends wpdb {
 		// Trim potential whitespace or subquery chars
 		$q = ltrim( $q, "\r\n\t (" );
 
-		// Possible writes
-		if ( preg_match( '/(?:ALTER|CREATE|ANALYZE|CHECK|OPTIMIZE|REPAIR|CALL|DELETE|DROP|INSERT|LOAD|REPLACE|UPDATE|SET|RENAME)\s/i', $q ) ) {
-			return true;
-		}
+		// Trim whitespaces and query delimiters ;
+		$q = rtrim( $q, "\r\n\t ;" );
 
 		// Not possible non-writes (phew!)
-		return ! preg_match( '/^(?:SELECT|SHOW|DESCRIBE|DESC|EXPLAIN)\s/i', $q );
+		return preg_match( '/^SELECT\s.+\sFOR\s+UPDATE$/i', $q )
+			|| ! preg_match( '/^(?:SELECT|SHOW|DESCRIBE|DESC|EXPLAIN)\s/i', $q );
 	}
 
 	/**
